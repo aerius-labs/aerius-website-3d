aa0f1a12391d41f45bea2710a1021142a57c74f4
AnkushS27 <ankush27singh11@gmail.com>
2024-06-03 17:30:41 +0530

    services section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import TopSection from '@/components/Section/topSection';
+import ServicesSection from '@/components/Section/servicesSection';
 import TrustSection from '@/components/Section/trustSection';
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between bg-black p-5 lg:p-10'>
+    <main className='flex flex-col items-center justify-between overflow-x-hidden bg-black p-5 lg:p-10'>
       <TopSection />
+      <ServicesSection />
       <TrustSection />
     </main>
   );
