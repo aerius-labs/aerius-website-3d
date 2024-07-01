0e2a0e69d191aec70e8e9d19cc5900cc2313ffab
AnkushS27 <ankush27singh11@gmail.com>
2024-07-01 15:33:36 +0530

    satellite model added and basic animation done

---
+++ src/components/scene/satelliteScene.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { Environment } from '@react-three/drei';
+import SatelliteModel from '../3dModel/satellite';
+
+export default function SatelliteScene() {
+  return (
+    <Canvas
+      gl={{ antialias: true }}
+      dpr={[1, 1.5]}
+      camera={{
+        far: 1000,
+        fov: 32.269,
+        near: 0.1,
+        position: [-28.552, -15.162, 0.847],
+        rotation: [1.253, -0.938, 1.184],
+      }}
+    >
+      <color attach='background' args={['#000']} />
+      <directionalLight position={[-5, -5, 5]} intensity={4} />
+      <Environment preset='night' />
+      <Suspense fallback={null}>
+        <SatelliteModel />
+      </Suspense>
+    </Canvas>
+  );
+}
