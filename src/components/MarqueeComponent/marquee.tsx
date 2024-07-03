b6d817d5316d89ce6ee2d69aa2cd78137714320d
AnkushS27 <ankush27singh11@gmail.com>
2024-07-03 16:55:10 +0530

    replaced divs with map in marquee component

--- src/components/MarqueeComponent/marquee.tsx
+++ src/components/MarqueeComponent/marquee.tsx
@@ -1,12 +1,20 @@
 const MarqueeComponent = () => {
+  const companies = [
+    'Company1',
+    'Company2',
+    'Company3',
+    'Company4',
+    'Company5',
+    'Company6',
+  ];
+
   return (
     <div className='flex w-screen justify-around border-b-2 border-t-2 border-white bg-black'>
-      <div className='p-4'>Company1</div>
-      <div className='p-4'>Company2</div>
-      <div className='p-4'>Company3</div>
-      <div className='p-4'>Company4</div>
-      <div className='p-4'>Company5</div>
-      <div className='p-4'>Company6</div>
+      {companies.map((company, index) => (
+        <div key={index} className='p-4'>
+          {company}
+        </div>
+      ))}
     </div>
   );
 };
