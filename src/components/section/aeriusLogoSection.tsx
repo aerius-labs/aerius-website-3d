847a92299cdb7bfb87bac6b24938389820a5ddb2
manavbabber <manavfazilka@gmail.com>
2025-01-01 12:46:39 +0530

    fix: aerius model to have fadein effect and shift it to top

--- src/components/section/aeriusLogoSection.tsx
+++ src/components/section/aeriusLogoSection.tsx
@@ -1,17 +1,48 @@
 import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const Scene = dynamic(() => import('@/components/scene/aeriusLogoScene'), {
   ssr: false,
 });
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function AeriusLogoSection() {
+  useEffect(() => {
+    const logoContainer = document.getElementById('logoContainer');
+
+    if (logoContainer) {
+      gsap.fromTo(
+        logoContainer,
+        { opacity: 0 },
+        {
+          opacity: 1,
+          scrollTrigger: {
+            trigger: '#logoSection',
+            start: 'top center',
+            end: 'top top',
+            scrub: true,
+          },
+        }
+      );
+    }
+
+    return () => ScrollTrigger.getAll().forEach((t) => t.kill());
+  }, []);
+
   return (
-    <div className='relative h-[150vh]'>
-      <div id='logoContainer' className='h-screen w-screen brightness-100'>
+    <div id='logoSection' className='relative h-[130vh]'>
+      <div
+        id='logoContainer'
+        className='sticky top-0 h-screen w-screen'
+        style={{ marginTop: '-10vh' }}
+      >
         <Scene />
       </div>
       <div
-        className='absolute bottom-0 left-0 right-0 h-[15vh] bg-gradient-to-t from-black via-black/50 to-transparent'
+        className='absolute bottom-0 left-0 right-0 h-[15vh]'
         style={{
           backgroundImage:
             'linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, .5) 47%, #000)',
